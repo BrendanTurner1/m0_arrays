ad17ff713c76493a27e10e528f0262c5b4e8a0f5
Brendan Turner <kisbyturner@gmail.com>
2023-08-01 16:33:24 -0600

    Adds statments that print array values

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var arrayStrings = ["zero", "one", "two", "three"];
 var arrayNumbers = [0, 1, 2, 3, 4];
 var arrayBoolean = [true, false, false, true];
+
+
+// This method will print the second and third value in the arrayString variable
+console.log(`The 2nd string is ${arrayStrings[1]}, and the third string is ${arrayStrings[2]}.`);
+
+// This method will print the last number in the arrayNumbers variable
+console.log(`The last number in the array is ${arrayNumbers[4]}`);
+
+// This method will print the first two values in the arrayBoolean varaible
+console.log(`The first boolean is ${arrayBoolean[0]}, and the second boolean is ${arrayBoolean[1]}`);
